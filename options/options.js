8d6ceb0fa88c032d70bd01d3e1f947170dc05d50
Crowdbotics <team@crowdbotics.com>
2024-02-17 07:58:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "simple-genai-chatbo-46438",
+  url: "https://simple-genai-chatbo-46438.botics.co",
+  api: "https://simple-genai-chatbo-46438.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
